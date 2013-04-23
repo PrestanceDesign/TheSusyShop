bbe39aa2b5f93049891fd689740be2a63031f90f
Valentin LÉVÊQUE <valentin@creativegang.fr>
2013-04-23 23:55:27 +0200

    Change css and sass dir

--- config.rb
+++ config.rb
@@ -4,9 +4,9 @@ require 'compass-normalize'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "sass"
-images_dir = "images"
+css_dir = "css"
+sass_dir = "css/src"
+images_dir = "img"
 javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
